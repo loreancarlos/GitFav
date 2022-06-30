ffdf9592d31a3eff2546c050d4047593e7ae50aa
Lorean FSC <lorean.carlos@gmail.com>
2022-06-30 00:56:23 -0300

    Almost finished

---
+++ js/favorites.js
@@ -0,0 +1,50 @@
+import { githubUser } from "./githubUser.js";
+
+export class Favorites {
+   constructor(root) {
+      this.root = document.querySelector(root);
+      this.tbody = this.root.querySelector(`table tbody`);
+      this.load();
+   }
+
+   load() {
+      this.favAccounts = JSON.parse(localStorage.getItem("@github-favorites:")) || [];
+   }
+
+   save() {
+      localStorage.setItem("@github-favorites:", JSON.stringify(this.favAccounts));
+   }
+
+   async add(username) {
+      try {
+         const user = await githubUser.search(username);
+
+         if (user.login === undefined) {
+            throw new Error("Este usuário não existe");
+         }
+
+         this.isAdded = false;
+         this.favAccounts.forEach(account => {
+            if (user.login.toLowerCase() == account.login.toLowerCase()) {
+               this.isAdded = true;
+            }
+         });
+
+         if (this.isAdded == true) {
+            throw new Error("Este usuário já foi favoritado.");
+         }
+
+         this.favAccounts = [user, ...this.favAccounts];
+         this.update();
+         this.save();
+      } catch (error) {
+         alert(error.message);
+      }
+   }
+
+
+   delete(user) {
+      this.favAccounts = this.favAccounts.filter(entry => entry.login !== user.login);
+      this.save();
+   }
+}
---
+++ js/favoritesView.js
@@ -0,0 +1,76 @@
+import { Favorites } from "./favorites.js";
+
+export class FavoritesView extends Favorites {
+   constructor(root) {
+      super(root);
+      this.update();
+      this.onAdd();
+   }
+
+   update() {
+      this.removeAllRow();
+      this.favAccounts.forEach(account => {
+         const row = this.createRow();
+         row.querySelector("td .user-wrapper img").src = `https://github.com/${account.login}.png`;
+         row.querySelector("td .user-wrapper img").alt = `Foto de ${account.name}`;
+         row.querySelector("td .user-wrapper .name-wrapper a").href = `https://github.com/${account.login}`;
+         row.querySelector("td .user-wrapper .name-wrapper a p:first-child").textContent = account.name;
+         row.querySelector("td .user-wrapper .name-wrapper a p:last-child").textContent = `/${account.login}`;
+         row.querySelector("td:nth-child(2)").textContent = account.public_repos;
+         row.querySelector("td:nth-child(3)").textContent = account.followers;
+         row.querySelector("td:last-child p").onclick = () => {
+            const isOK = confirm("Tem certeza que deseja remover este usuário da sua lista de Favoritos?");
+            if (isOK) {
+               this.delete(account);
+               this.update();
+            }
+         };
+         this.tbody.append(row);
+      });
+   }
+
+   createRow() {
+      const tr = document.createElement("tr");
+      tr.innerHTML = ` 
+      <td>
+         <div class="user-wrapper">
+            <img src="" alt="">
+            <div class="name-wrapper">
+               <a href="" target="_blank">
+                  <p></p>
+                  <p></p>
+               </a>
+            </div>
+         </div>
+      </td>
+      <td></td>
+      <td></td>
+      <td>
+         <p>Remover</p>
+      </td>
+      `;
+      return tr;
+   }
+
+   removeAllRow() {
+      this.tbody.querySelectorAll("tr").forEach(row => {
+         row.remove();
+      });
+   }
+
+   onAdd() {
+      const buttonSearch = this.root.querySelector("header .search-wrapper button");
+      const inputSearch = this.root.querySelector("header .search-wrapper input");
+
+      buttonSearch.onclick = () => {
+         const { value } = inputSearch;
+         this.add(value);
+      };
+
+      inputSearch.onkeydown = (event) => {
+         if (event.keyCode === 13) {
+            buttonSearch.onclick();
+         }
+      };
+   }
+}
